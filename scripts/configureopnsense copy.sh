cc4ad177624064dc98d3b60f36504fbdcc37188d
Daniel Mauser <dmauser@hotmail.com>
2025-02-28 09:00:34 -0600

    update

--- scripts/configureopnsense copy.sh
+++ scripts/configureopnsense copy.sh
@@ -51,38 +51,28 @@ pkg bootstrap -f; pkg update -f
 env ASSUME_ALWAYS_YES=YES pkg install ca_root_nss && pkg install -y bash
 
 #Download OPNSense Bootstrap and Permit Root Remote Login
-# fetch https://raw.githubusercontent.com/opnsense/update/master/src/bootstrap/opnsense-bootstrap.sh.in
-#fetch https://raw.githubusercontent.com/opnsense/update/7ba940e0d57ece480540c4fd79e9d99a87f222c8/src/bootstrap/opnsense-bootstrap.sh.in
-fetch https://raw.githubusercontent.com/opnsense/update/master/src/bootstrap/opnsense-bootstrap.sh.in
+fetch https://raw.githubusercontent.com/opnsense/update/7ba940e0d57ece480540c4fd79e9d99a87f222c8/src/bootstrap/opnsense-bootstrap.sh.in
+#fetch https://raw.githubusercontent.com/opnsense/update/master/src/bootstrap/opnsense-bootstrap.sh.in
 sed -i "" 's/#PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config
 
 #OPNSense
-# Due to a recent change in pkg the following commands no longer finish with status code 0
-#		pkg unlock -a
-#		pkg delete -fa
-# This resplace of set -e which force the script to finish in case of non status code 0 has to be inplace
-sed -i "" "s/set -e/#set -e/g" opnsense-bootstrap.sh.in
 sed -i "" "s/reboot/shutdown -r +1/g" opnsense-bootstrap.sh.in
-sh ./opnsense-bootstrap.sh.in -y -r "25.1"
+sh ./opnsense-bootstrap.sh.in -y -r "21.7"
 
 # Add Azure waagent
-fetch https://github.com/Azure/WALinuxAgent/archive/refs/tags/v2.12.0.4.tar.gz
-tar -xvzf v2.12.0.4.tar.gz
-cd WALinuxAgent-2.12.0.4/
+fetch https://github.com/Azure/WALinuxAgent/archive/refs/tags/v2.4.0.2.tar.gz
+tar -xvzf v2.4.0.2.tar.gz
+cd WALinuxAgent-2.4.0.2/
 python3 setup.py install --register-service --lnx-distro=freebsd --force
 cd ..
 
 # Fix waagent by replacing configuration settings
-ln -s /usr/local/bin/python3.11 /usr/local/bin/python
-##sed -i "" 's/command_interpreter="python"/command_interpreter="python3"/' /etc/rc.d/waagent
-##sed -i "" 's/#!\/usr\/bin\/env python/#!\/usr\/bin\/env python3/' /usr/local/sbin/waagent
+ln -s /usr/local/bin/python3.8 /usr/local/bin/python
+#sed -i "" 's/command_interpreter="python"/command_interpreter="python3"/' /etc/rc.d/waagent
+#sed -i "" 's/#!\/usr\/bin\/env python/#!\/usr\/bin\/env python3/' /usr/local/sbin/waagent
 sed -i "" 's/ResourceDisk.EnableSwap=y/ResourceDisk.EnableSwap=n/' /etc/waagent.conf
 fetch $1actions_waagent.conf
 cp actions_waagent.conf /usr/local/opnsense/service/conf/actions.d
-
-# Installing bash - This is a requirement for Azure custom Script extension to run
-pkg install -y bash
-pkg install -y os-frr
 
 # Remove wrong route at initialization
 cat > /usr/local/etc/rc.syshook.d/start/22-remoteroute <<EOL
@@ -128,9 +118,3 @@ echo static_arp_azvip=\"168.63.129.16 12:34:56:78:9a:bc\" >> /etc/rc.conf
 service static_arp start
 # To survive boots adding to OPNsense Autorun/Bootup:
 echo service static_arp start >> /usr/local/etc/rc.syshook.d/start/20-freebsd
-
-# Reset WebGUI certificate
-echo #\!/bin/sh >> /usr/local/etc/rc.syshook.d/start/94-restartwebgui
-echo configctl webgui restart renew >> /usr/local/etc/rc.syshook.d/start/94-restartwebgui
-echo rm /usr/local/etc/rc.syshook.d/start/94-restartwebgui >> /usr/local/etc/rc.syshook.d/start/94-restartwebgui
-chmod +x /usr/local/etc/rc.syshook.d/start/94-restartwebgui
